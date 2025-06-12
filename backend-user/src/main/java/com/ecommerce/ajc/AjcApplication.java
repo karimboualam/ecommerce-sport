6d555c9fa6819caa77519f9ab6c0b5b82f58f503
Karim <boualam.karim94@gmail.com>
2025-06-12 15:28:37 +0200

    initialisation du projet back

---
+++ backend-user/src/main/java/com/ecommerce/ajc/AjcApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ajc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjcApplication.class, args);
+	}
+
+}
---
+++ backend-user/src/test/java/com/ecommerce/ajc/AjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ajc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
